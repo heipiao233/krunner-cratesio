1825085fd86b227b712cf71ff14324efa504e833
Hei Piao <heipiao233@outlook.com>
2024-04-07 20:08:00 +0800

    Add search.
    Signed-off-by: Hei Piao <heipiao233@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::fmt::Display;
+use std::{fmt::Display, iter::once};
 
 use crates_io::{Crate, Registry};
 use curl::easy::Easy;
@@ -6,11 +6,11 @@ use krunner::{Match, MatchIcon, MatchType, RunnerExt};
 
 #[derive(krunner::Action, Debug, Clone)]
 enum Action {
-    #[action(id = "cratesio", title = "Show in crates.io", icon = "cratesio")]
+    #[action(id = "cratesio", title = "crates.io", icon = "cratesio")]
     CratesIo,
-    #[action(id = "docsrs", title = "Show in docs.rs", icon = "docsrs")]
+    #[action(id = "docsrs", title = "docs.rs", icon = "docsrs")]
     DocsRs,
-    #[action(id = "librs", title = "Show in lib.rs", icon = "librs")]
+    #[action(id = "librs", title = "lib.rs", icon = "librs")]
     LibRs,
 }
 
@@ -34,26 +34,26 @@ impl Display for Error {
 }
 
 fn crate_to_match(from: &Crate, query: String) -> Match<Action> {
-	let ty = if from.name == query {
-		MatchType::ExactMatch
-	} else {
-		MatchType::PossibleMatch
-	};
+    let (ty, relevance) = if from.name == query {
+        (MatchType::ExactMatch, 0.7)
+    } else {
+        (MatchType::PossibleMatch, 0.5)
+    };
     Match {
         id: from.name.clone(),
         title: from.name.clone(),
         subtitle: from.description.clone(),
         icon: MatchIcon::ByName("cratesio".to_string()),
         ty,
-        relevance: 0.5,
-        urls: [
-			format!("https://crates.io/crate/{}", from.name),
-			format!("https://lib.rs/{}", from.name),
-			format!("https://docs.rs/{}", from.name),
-		].to_vec(),
+        relevance: relevance,
+        urls: vec![
+            format!("https://crates.io/crate/{}", from.name),
+            format!("https://lib.rs/{}", from.name),
+            format!("https://docs.rs/{}", from.name),
+        ],
         category: None,
         multiline: false,
-        actions: [Action::CratesIo, Action::DocsRs, Action::LibRs].to_vec(),
+        actions: vec![Action::CratesIo, Action::DocsRs, Action::LibRs],
     }
 }
 
@@ -62,29 +62,46 @@ impl krunner::Runner for Runner {
     type Err = Error;
 
     fn matches(&mut self, query: String) -> Result<Vec<Match<Self::Action>>, Self::Err> {
-		if !query.starts_with("crate ") {
-			return Ok(Vec::new());
-		}
-		let query = query[6..].to_string();
-		println!("{}", query);
-        let matches = self.0
-            .search(query.as_str(), 10)?
-            .0;
-		println!("{:?}", matches.iter().map(|i|i.name.clone()).collect::<Vec<String>>());
-		let matches = matches
+        if !query.starts_with("crate ") {
+            return Ok(Vec::new());
+        }
+        let query = query[6..].to_string();
+        let matches = self.0.search(query.as_str(), 10)?.0;
+        let matches = matches
             .iter()
             .map(|i| crate_to_match(i, query.clone()))
+            .chain(once(Match {
+                id: format!("search {query}"),
+                title: format!("Search {query}"),
+                subtitle: None,
+                icon: MatchIcon::ByName("search-icon".to_string()),
+                ty: MatchType::HelperMatch,
+                relevance: 0.6,
+                urls: vec![
+                    format!("https://crates.io/search?q={query}"),
+                    format!("https://docs.rs/releases/search?query={query}"),
+                    format!("https://lib.rs?{query}"),
+                ],
+                category: None,
+                multiline: false,
+                actions: vec![Action::CratesIo, Action::DocsRs, Action::LibRs],
+            }))
             .collect();
-
+        println!("{:#?}", matches);
         Ok(matches)
     }
 
     fn run(&mut self, match_id: String, action: Option<Self::Action>) -> Result<(), Self::Err> {
-        match action {
-			Some(Action::CratesIo) => open::that(format!("https://crates.io/crates/{match_id}"))?,
-			Some(Action::DocsRs) => open::that(format!("https://docs.rs/{match_id}"))?,
-			Some(Action::LibRs) | None => open::that(format!("https://lib.rs/{match_id}"))?,
-		}
+        open::that(match (action, match_id.strip_prefix("search ")) {
+            (Some(Action::CratesIo), Some(query)) => format!("https://crates.io/search?q={query}"),
+            (Some(Action::DocsRs), Some(query)) => {
+                format!("https://docs.rs/releases/search?query={query}")
+            }
+            (Some(Action::LibRs) | None, Some(query)) => format!("https://lib.rs?{query}"),
+            (Some(Action::CratesIo), None) => format!("https://crates.io/crates/{match_id}"),
+            (Some(Action::DocsRs), None) => format!("https://docs.rs/{match_id}"),
+            (Some(Action::LibRs) | None, None) => format!("https://lib.rs/{match_id}"),
+        })?;
         Ok(())
     }
 }
